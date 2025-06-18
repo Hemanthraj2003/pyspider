64eb0fa5aed81b1c69e56a0e031e0fb237ceca11
Hemanthraj2003 <hemanth3raj@gmail.com>
2025-06-18 13:30:32 +0530

    logical operators

---
+++ pythonBasic/Operators/LogicalOperator.py
@@ -0,0 +1,16 @@
+'''
+python has 3 logical operators
+
+# and
+# or
+# not
+note: these are lowercase
+
+these are used to compare two or more conditions and return True Or False based on the opearator used
+
+'''
+
+a,b,c = 10, 20,10 #unpacking
+print(a==b and a==c) # output False
+print(a==b or a==c) # output True
+print(not a==c) # output False
