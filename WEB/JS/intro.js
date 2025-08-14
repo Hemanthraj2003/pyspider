6438d01a50c497ebc3bd9e32fde0470ac9026bb6
Hemanthraj2003 <hemanth3raj@gmail.com>
2025-08-14 10:30:03 +0530

    JS:Intro To JavaScript

---
+++ WEB/JS/intro.js
@@ -0,0 +1,38 @@
+/**
+ * JAVASCRIPT 
+ * IS A CLINET AND SERVER SIDE SCRITPTING LAGUAGE
+ * with respect to front-end javascript is used for dynamic and validation purposes
+ * 
+ * CLINET SIDE AND SERVER SIDE ARCHITECTURE
+ * 
+ * 
+ * HISTORY
+ * JS is introduced my "Brendom EICH" in the year 1995
+ * it is maintained by ECMA(European Computer Manufacture Association) from the year 1997
+ * we have different version of JS, i.e., ES-1 to ES-6 (ES - ECMA Script)
+ * 
+ * 
+ * NOTE : feature
+ *  - JavaScript mainly used to convert static webpages to dynamic webpages
+ * 
+ * JS code will be executed by JAVA SCRIPT ENGINE which is integrated in ALL Browser's
+ *    chrome - V8 Engine
+ *    I.E    - chakra
+ *    firefox - spyder monkey
+ * 
+ * 
+ * TYPES OF JAVASCRIPT
+ *  
+ * -- internal javascript 
+ * 
+ *  Writing javascript code in the same HTML Script Tag is called Internal Javascript
+ *  ex: <script>--- JS Code ---</script>   
+ *    
+ * -- external javascript
+ * 
+ * Writing javascript in a seperate .js file and linking that file using <script src="JS-FILE link" >
+ * ex: <script src="--PATH--"></script>
+ * 
+ * 
+ *  
+*/
