912d2aa2d0ca7d1a8446cebbeaf2dbf9d93aca94
Hemanthraj2003 <hemanth3raj@gmail.com>
2025-08-20 18:26:50 +0530

    js ops

--- WEB/JS/Operator.js
+++ WEB/JS/Operator.js
@@ -43,12 +43,56 @@ console.log(`My Name is ${fname} ${lname}, Im from ${place}, My age is ${age}`);
  *  1 && 1 = 1
  *  0 && 1 = 0
  *  1 && 0 = 0
- *  0
- * 
- 
- 
- 
- 
- 
-  && 0 = 0
+ *  0 && 0 = 0
+ */
+
+/**
+ * BITWISE OPERATOR
+ *
+ * bitwise & (and)   - bitwise | (or)
+ *
+ * will perform operations on binary value
+ * decimal value is converted into binary value and operations are performed and rsults will converted back to decimal number
+ *
+ */
+
+/**
+ * ASSIGNMENT OPERATOR (=)
+ *  assigns the value of right-hand operator to a variable
+ * the results of an assignment expression is the value assigned to the left-hand operand
+ * also includes ( +=, -=, *=, /=, %= ) we called it as short-hand operator
+ * short-hand operator: combining arithematic operator (+,-,*,/,%) and assignment operator
+ * Assign the result value in same variable after performing some operation on that variable i.e., (+=, -=, *= , /=, %=)
+ *
+ * examples : x+=5, x-=5, x/=2,x*=8
+ *
+ */
+
+/**
+ * UNARY OPERATOR
+ * A unary operation is an operation with only one operand
+ * this operand comes either before or after the operator to increment(++) or decrement(--) the values.
+ *
+ * ++
+ * postIncriment a++ store 1st and then increment
+ * preIncriment ++a  increment 1st and then store
+ *
+ * --
+ * postDec a-- store 1st then dec
+ * preDec --a  dec first and then store
+ *
+ * NOTE: unary operator should not be used
+ */
+
+/**
+ * TURNARY OPERATOR
+ *
+ * the conditional operator is the operator that takes 3 operands
+ * 1. a condition
+ * 2. true statement
+ * 3. false statement
+ *
+ * syntax:    condition ? statement1: statement2;
+ *
+ *    ; is important to be closed
  */
